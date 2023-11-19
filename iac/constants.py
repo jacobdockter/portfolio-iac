dff0e2736510538a1d9a6e7acfb2ba75ed660fa0
Jacob Dockter <dockterboy4@gmail.com>
2023-11-18 22:31:10 -0700

    update default credentials

--- iac/constants.py
+++ iac/constants.py
@@ -1,6 +1,6 @@
 """constants.py
 """
-CODESTAR_ARN = "arn:aws:codestar-connections:us-east-1:519292530037:connection/5b4f0d41-8af8-49b2-918b-8955b97aee9d"
+CODESTAR_ARN = "arn:aws:codestar-connections:us-west-2:531120402063:connection/eff81f9d-3087-4f5a-b273-89f91ca7be75"
 GITHUB_ACCOUNT = "jacobdockter"
-DOMAIN_ZONE_ID = "Z04599073ANR3P6TNUUQD"
+DOMAIN_ZONE_ID = "Z08526392X7SJ3GFLY2GT"
 BASE_DOMAIN = "jacobdockter.com"
